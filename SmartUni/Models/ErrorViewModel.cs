6afab9f123d74caaad06bf60498f9313694b484a
Raystern <raysternwong@gmail.com>
2019-08-02 14:11:02 +0800

    添加项目文件。

---
+++ SmartUni/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartUni.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
